05384d61e9ebc39ecddcdaa8249985825afbf310
Marcelo Soares <marcelo.soares92@gmail.com>
2023-07-02 16:12:30 -0300

    Document Object Model

---
+++ Laboratório/JAVASCRIPT/Exercício_13.js
@@ -0,0 +1,24 @@
+/*
+Autor: Marcelo Soares
+Data: 2023-07-02
+Exercício 13: DOM (Document Object Model)
+*/
+//
+// Quando uma página HTML é carregada, o navegador cria um DOM da página cujo pai é "#document"
+// O modelo HTML DOM é construido como uma árvore de objetos.
+//
+// Com o modelo de objeto, JavaScript tem todo o poder necessário para criar HMTL dinamicamente:
+// O JavaScript pode:
+// > alterar todos os elementos HTML na página
+// > alterar todos os atributos dos elementos HTML na página
+// > alterar todos os estilos CSS
+// > remover um elemento HTML e seus atributos
+// > adicionar um elemento HTML e seus atributos
+// > reagir a todos os eventod que ocorrem em uma página
+// > pode criar novos eventos na página
+//
+// Window - nosso primeiro elemento da árvire do DOM. Exemplos:
+//
+// window.alert() - cria uma janela de alerta com o botão "ok"
+// window.confirm() - cria uma janela com as opções "ok" e "cancel"
+// window.prompt() - cria uma janela para imput de dados com o botão "ok"
