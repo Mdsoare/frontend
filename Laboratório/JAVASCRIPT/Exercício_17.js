6cf5b875189a1a33fce71ff9242a54ea30a6cebb
Marcelo Soares <marcelo.soares92@gmail.com>
2023-07-04 20:39:46 -0300

    Trabalhando com eventos

---
+++ Laboratório/JAVASCRIPT/Exercício_17.js
@@ -0,0 +1,17 @@
+/*
+Autor: Marcelo Soares
+Data: 2023-07-04
+Exercício 17: Trabalhando com eventos
+*/
+
+// O que é um evento?
+//
+// A interação do JavaScript com HTML é tratada por meio de eventos que ocorrem quando o usuário ou o navegador manipula uma página.
+//
+// Quando a página é carregada, ela é chamada de evento
+//
+// Quando o usuário clica em um botão, esse clique também é um evento. Outros exemplos incluem como pressionar qualquer tecla, fechar uma janela,
+// redimencionar uma janela, etc.
+//
+// Os desenvolvedores podem utilizar esses eventos para executar respostas codificadas em JavaScript, o que faz com que os botões fechem as janelas,
+// as mensagens sejam exibidas aos usuários, os dados sejam validados e praticamente qualquer outro tipo de resposta que você possa imaginar.
